188211f9ad5a023b55901ae343548c05f9157fca
Dattatray <dattatrayh@gmail.com>
2021-03-16 14:23:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,278 @@
+<?php 
+$desc = "Experience the faster, safer, more affordable and completely decentralized enterprise-grade cloud storage network.";
+$title = "StorX | Distributed Cloud Storage with Blockchain Technology";
+
+include('inc/header.php') ?>
+
+<!-- Banner Area -->
+<section id="" class="banner_area ba_3">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 banner_con">
+                <h2 class="wow fadeInUp">Distributed Cloud Storage Network</h2>
+                <p class="wow fadeInUp" data-wow-delay="0.3s">Experience the faster, safer, more affordable and completely decentralized enterprise-grade cloud storage network.</p>
+                <!--<a href="#" class="theme_btn wow fadeInUp" data-wow-delay="0.7s">Create Account</a>-->
+            </div>
+            <div class="col-lg-6">
+                <div class="banner_i wow fadeInUp">
+                    <img src="assets/images/cloud-storage-place.png" alt="" class="images" />
+                </div>
+                <img src="assets/images/cloud-storage-place.png" alt="" class="rs_banner wow fadeInUp" />
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Banner Area -->
+
+<!-- About Cloud Storage -->
+<section id="about" class="welcome_content pt-5">
+    <div class="container">
+        <h2 class="wow fadeInUp">Truly Decentralized Cloud Storage</h2>
+        <p class="t_p wow fadeInUp" data-wow-delay="0.3s">
+            StorX helps you securely encrypt, fragment and then distribute important data across multiple hosting nodes spread worldwide. StorX provides a democratic marketplace for hosting data, replacing the centralized intermediaries
+            with a decentralized blockchain network. Exercise complete control and grant access over your data sets without any centralized intermediary.
+        </p>
+    </div>
+</section>
+<!-- About Cloud Storage -->
+
+<!-- Pricing Comparison -->
+<section id="pricing" class="pricing_area">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-4 mb-sx-4">
+                <h2 class="wow fadeInUp">Cloud Storage Pricing Comparison</h2>
+                <p class="t_p wow fadeInUp" data-wow-delay="0.3s">See how AWS S3, Azure, and Google Cloud compare to StorX Cloud Storage.</p>
+            </div>
+
+            <div class="col-lg-8">
+                <div id="pricing-table-block" class="wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="table-responsive">
+                        <table class="table pricing-table">
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="fw-5">Provider</th>
+                                    <th scope="col">
+                                        <img src="assets/images/icons/storage.png" width="40" height="40" /><br />
+                                        <span class="fw-5">Storage</span><br />
+                                        ($/GB/Month)
+                                    </th>
+                                    <th scope="col">
+                                        <img src="assets/images/icons/download.png" width="40" height="40" /><br />
+                                        <span class="fw-5">Download</span><br />
+                                        ($/GB)
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="primary-bg">
+                                    <th scope="row"><img src="assets/images/pricing-logos/storx-logo.png" class="img-responsive b2-comparison-logo" style="width: 125px; height: 44px;" alt="Store X Cloud Storage" /></th>
+                                    <td class="bh" style="color: #2aa72a;">$0.010</td>
+                                    <td class="bh" style="color: #2aa72a;">$0.02</td>
+                                </tr>
+                                <tr class="secondary-bg">
+                                    <th scope="row"><img src="assets/images/pricing-logos/amazon-logo.png" class="img-responsive b2-comparison-logo" style="width: 125px; height: 44px;" alt="Amazon S3 Storage" /></th>
+                                    <td style="line-height: 20px;">
+                                        $0.021<br />
+                                        <span class="comparison-blue">+320%</span>
+                                    </td>
+                                    <td>
+                                        $0.09<br />
+                                        <span class="comparison-blue">+800%</span>
+                                    </td>
+                                </tr>
+                                <tr class="secondary-bg">
+                                    <th scope="row"><img src="assets/images/pricing-logos/microsoft-logo.png" class="img-responsive b2-comparison-logo" style="width: 125px; height: 44px;" alt="Microsoft Azure Cloud Storage" /></th>
+                                    <td>
+                                        $0.018<br />
+                                        <span class="comparison-blue">+260%</span>
+                                    </td>
+                                    <td>
+                                        $0.087<br />
+                                        <span class="comparison-blue">+770%</span>
+                                    </td>
+                                </tr>
+                                <tr class="secondary-bg">
+                                    <th scope="row"><img src="assets/images/pricing-logos/google-cloud-logo.png" class="img-responsive b2-comparison-logo" style="width: 125px; height: 44px;" alt="Google Cloud Storage" /></th>
+                                    <td>
+                                        $0.020 <br />
+                                        <span class="comparison-blue">+300%</span>
+                                    </td>
+                                    <td>
+                                        $0.12<br />
+                                        <span class="comparison-blue">+1100%</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Pricing Comparison -->
+
+<!-- Experience Decentralized Cloud Storage -->
+<section class="experience_cta_block">
+    <div class="container">
+        <h2 class="wow fadeInUp">Experience Decentralized Cloud Storage</h2>
+        <a href="#" class="theme_btn wow fadeInUp" data-wow-delay="0.1s">Host a Node</a>
+        <a href="#" class="theme_btn create wow fadeInUp" data-wow-delay="0.2s">Store Data</a>
+    </div>
+</section>
+<!-- Experience Decentralized Cloud Storage -->
+
+<!-- Why StorX -->
+<section id="whyStorX" class="why_storx">
+    <div class="container">
+        <h2 class="wow fadeInUp">Why StorX?</h2>
+        <div class="row d-flex services_row">
+            <!-- services_item -->
+            <div class="col-lg-6 col-md-6 mb-4 wow fadeInUp" data-wow-delay="0.2s">
+                <div class="services_item">
+                    <span class="img_c"><i class="fa fa-thumbs-up"></i> Reliable Platform</span>
+
+                    <ul class="introducing-list">
+                        <li class="introducing-list-item">Thoroughly Tested for Quality Assurance</li>
+                        <li class="introducing-list-item">Dedicated support delivered directly by our engineers</li>
+                        <li class="introducing-list-item">Work faster and collaborate seamlessly</li>
+                        <li class="introducing-list-item">Enterprise grade SLA up to 24/7</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- services_item -->
+            <div class="col-lg-6 col-md-6 mb-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="services_item">
+                    <span class="img_c i-2"><i class="fas fa-medal"></i> Enterprise-Grade</span>
+                    <ul class="introducing-list">
+                        <li class="introducing-list-item">Enterprise-grade security features and hardening</li>
+                        <li class="introducing-list-item">Early notifications about critical security issues</li>
+                        <li class="introducing-list-item">Early security patches</li>
+                        <li class="introducing-list-item">Pro active security consulting to review and harden your instance</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- services_item -->
+            <div class="col-lg-6 col-md-6 mb-4 wow fadeInUp" data-wow-delay="0.4s">
+                <div class="services_item">
+                    <span class="img_c i-3"><i class="fa fa-check"></i> Expertise</span>
+                    <ul class="introducing-list">
+                        <li class="introducing-list-item">Our experts help you migrate to StorX</li>
+                        <li class="introducing-list-item">Access to documentation and the knowledge base</li>
+                        <li class="introducing-list-item">Access to expertise around scaling, security and best practices</li>
+                        <li class="introducing-list-item">Access to optional workshops and training</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- services_item -->
+            <div class="col-lg-6 col-md-6 mb-4 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="services_item">
+                    <span class="img_c i-4"><i class="fa fa-shield-alt"></i> Advanced Security</span>
+                    <ul class="introducing-list">
+                        <li class="introducing-list-item">Encrypt and Safely Distribute Data Across Multiple Nodes on Decentralized Network</li>
+                        <li class="introducing-list-item">Secure your data against internal and external security threats</li>
+                        <li class="introducing-list-item">Securely access data across multiple device</li>
+                        <li class="introducing-list-item">Share securely with confidence and ease</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Why StorX -->
+
+<!-- Smarter way to collect  -->
+<section class="decentralized_solution">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 smarter_text">
+                <h2 class="wow fadeInUp">StorX Decentralized Cloud Storage Network</h2>
+                <p class="wow fadeInUp" data-wow-delay="0.1s">
+                    StorX offers a decentralized cloud storage platform that connects users who need file storage with hosts worldwide offering underutilized storage capacity. Build on XinFin (XDC) Blockchain Network makes data completely
+                    secured, No intermediaries ensure economics for users and hosts.
+                </p>
+                <div class="media wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="media-left">
+                        <i class="far fa-eye"></i>
+                    </div>
+                    <div class="media-body">
+                        <div class="heding">Our Mission</div>
+                        <p>Worlds most reliable decentralized cloud services.</p>
+                    </div>
+                </div>
+                <div class="media wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="media-left">
+                        <i class="fas fa-microphone c_2"></i>
+                    </div>
+                    <div class="media-body">
+                        <div class="heding">Our Principles</div>
+                        <p>100% Decentralization.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 smarter_img">
+                <img src="assets/images/ex-img.png" alt="" />
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Smarter way to collect -->
+
+<!-- Features -->
+<section id="features" class="welcome_content">
+    <div class="container">
+        <h2 class="wow fadeInUp">Privacy and Security</h2>
+        <p class="wow fadeInUp" data-wow-delay="0.2s">
+            StorX encrypts and distributes the files across decentralized network. We believe that privacy is a fundamental human right. Everyone has the right of a secure communication. It's the main motivation behind all our products.
+        </p>
+        <h5 class="t_p mt-5 wow fadeInUp" data-wow-delay="0.4s">Blockchain technology ensures that your data is stored with 100% reliability.</h5>
+    </div>
+</section>
+<!-- Features -->
+
+<!-- Platform Features -->
+<section class="platform_features pf_3 pf_4 pt-2">
+    <div class="container">
+        <div class="row platform_row">
+            <div class="col-lg-4 col-md-4 wow fadeInUp">
+                <div class="platform">
+                    <div class="icons">
+                        <i class="flaticon-budget"></i>
+                    </div>
+                    <h3>Affordability</h3>
+                    <p>
+                        Due to its undermining distributed architecture, StorX is able to offer storage at almost 80 - 90% less compared to enterprise cloud storage peers. Storing 1TB of files on StorX costs about $ 2 - 4 per month,
+                        compared with $23 on Amazon S3.
+                    </p>
+                </div>
+            </div>
+            <!-- platform -->
+            <div class="col-lg-4 col-md-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="platform">
+                    <div class="icons i_2">
+                        <i class="flaticon-rule"></i>
+                    </div>
+                    <h3>Open source</h3>
+                    <p>
+                        Everything we do is 100% open source. We do not do open core or proprietary extensions. We use and support open standards as much as possible. We don't lock people in to proprietary formats. You can import and export
+                        everything.
+                    </p>
+                </div>
+            </div>
+            <!-- platform -->
+            <div class="col-lg-4 col-md-4 wow fadeInUp" data-wow-delay="0.6s">
+                <div class="platform">
+                    <div class="icons i_3">
+                        <i class="flaticon-collaboration"></i>
+                    </div>
+                    <h3>Community Development</h3>
+                    <p>StorX is able to offer new improved features due to ongoing support from its community. Our community members empower our platform and contribute.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Platform Features -->
+
+<?php include('inc/footer.php') ?>

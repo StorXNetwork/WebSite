a30fe4972532c966564f59a132ea3f5ca10a174c
Dattatray <dattatrayh@gmail.com>
2021-03-23 20:06:02 +0530

    Updates footer in all pages

--- host-node.php
+++ host-node.php
@@ -22,56 +22,6 @@ Join today to earn STORX.</p>
         </section>
         <!-- Banner Area -->
 
-        <!-- Contribute Popup Form -->
-        <div id="contribute-popup" class="white-popup mfp-hide">
-            <div class="lc_2">
-                <form class="row contribution_form">
-                    <div class="col-12">
-                        <h2 class="pt-3">Contribute Now!</h2>
-                        <p>There are many areas in StorX you can get involved in. From technical tasks like building an app, design, coding on the front or back-end to translation and helping out on the forums.</p>
-                        <p>We have a number of people who are happy to help you get started!</p>
-                    </div>
-
-                    <div class="form-group col-md-6">
-                        <input type="text" class="form-control" placeholder="Your Name" />
-                    </div>
-                    <div class="form-group col-md-6">
-                        <input type="email" class="form-control" name="email" placeholder="Your  Email Address" />
-                    </div>
-                    <div class="form-group col-12">
-                        <textarea class="form-control" id="message" name="message" placeholder="Your Contribution"></textarea>
-                    </div>
-                    <div class="form-group larg_btn m-0 col-12">
-                        <button class="theme_btn" type="submit">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-
-        <!-- Faq Area -->
-        <section class="faq_area fa_2">
-            <div class="container">
-                <h2>Frequently Asked Questions</h2>
-                <div class="accordion frequently">
-                    <div class="faq">
-                        <a href="#" class="collapsed" data-toggle="collapse" data-target="#faq_1">1. I am not a Software Developer, Can I still contribute?</a>
-                        <div id="faq_1" class="collapse" data-parent=".frequently">
-                            <p>You're Welcome! Programming is only one part of StorX – There are other important areas like UI/UX/Design, Social Media Marketing/Documentation, Video Repository, Host Events, and many other areas!</p>
-                            <p>We are also open to suggestions, If you feel you can add value to the community you can contribute. Do not hesitate to apply and tell us what you'd like to help with!</p>
-                        </div>
-                    </div>
-                    <div class="faq">
-                        <a href="#" class="collapsed" data-toggle="collapse" data-target="#faq_2">2. Am I welcome if I do not have a lot of experience and/or skills in Development?</a>
-                        <div id="faq_2" class="collapse" data-parent=".frequently">
-                            <p>Absolutely. We all started somewhere, and we welcome all levels of experience. This is why we have mentors: We want to help you to improve and learn from the community while contributing to StorX!</p>
-                            <p>Whether you have doubts about your skills or not, just apply and we will figure it out.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- End Faq Area -->
-
         <!-- Experience Decentralized Cloud Storage -->
         <section class="experience_cta_block">
             <div class="container">
